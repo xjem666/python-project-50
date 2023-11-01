702b6920e510ef1f283c5a2f3adfc80593e29432
Daniil Petrov <xjem666@yandex.ru>
2023-11-01 18:40:37 +0300

    add generate diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -23,3 +23,15 @@ def generate_diff(filepath1, filepath2):
             diff['+' + key] = data2[key]
 
     return diff
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff between two files')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
