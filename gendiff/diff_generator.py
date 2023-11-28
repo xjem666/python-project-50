e7a9a49bed8144e12318c3e8b067b0862f485584
Petrov Daniil <139789068+xjem666@users.noreply.github.com>
2023-11-28 16:10:50 +0300

    fix diff generator

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -1,4 +1,3 @@
-from gendiff.booleans_cleaner import format_bool_from_python_to_json
 
 from gendiff.data_reader import read_data
 from gendiff.data_reader import define_extension
@@ -11,7 +10,7 @@ def generate_diff(file_path1, file_path2, formatter='stylish'):
     file1 = loader(read_data(file_path1), define_extension(file_path1))
     file2 = loader(read_data(file_path2), define_extension(file_path2))
 
-    formatted_file1 = format_bool_from_python_to_json(file1)
-    formatted_file2 = format_bool_from_python_to_json(file2)
+    formatted_file1 = (file1)
+    formatted_file2 = (file2)
 
     return select_formats(formatted_file1, formatted_file2, formatter)
